f07925796a8c9fe3192b89727746e1d3c07f0e9c
LaithMahdi <mahdilaith380@gmail.com>
2025-12-01 14:40:11 +0100

    first commit

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primary = Color(0xFF246BFD);
+  static const Color secondary = Color(0xFF3EEBB4);
+  static const Color lineYellow = Color(0xFFEFFF53);
+  static const Color darkGray = Color(0xFF212121);
+  static const Color lightGray = Color(0xFF9E9E9E);
+  static const Color background = Color(0xFFFFFFFF);
+}
